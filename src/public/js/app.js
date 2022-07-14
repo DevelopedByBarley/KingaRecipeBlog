fdb042272d807e6aa3cc9cab1f350c6973c5bc5c
Árpád <developedbybarley@gmail.com>
2022-07-14 19:24:39 +0200

    Recipes form is updated

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -21,26 +21,34 @@ for (let deleteBtn of deleteRecipeBtn) {
 
 let ingredientsContainer = document.getElementById('ingredients-container')
 let addIngredients = document.getElementById('addIngredients');
+let removeIngredients = document.getElementById('removeIngredients');
 let ingredients = document.getElementById('ingredients')
 addIngredients.addEventListener('click', (event) => {
   event.preventDefault();
-  if (ingredients.value === '' || ingredients.value === null) {
-    alert('A mező kitöltése kötelező!')
-  } else {
-    let clone = ingredients.cloneNode(true);
-    clone.value = '';
-    ingredientsContainer.appendChild(clone);
-    
-  }
+  let clone = ingredients.cloneNode(true);
+  clone.value = '';
+  ingredientsContainer.appendChild(clone);
+
+})
+
+removeIngredients.addEventListener('click', (event) => {
+  event.preventDefault();
+  ingredientsContainer.removeChild(ingredientsContainer.lastChild)
 })
 
 let stepsContainer = document.getElementById('steps-container');
 let steps = document.getElementById('steps');
 let addSteps = document.getElementById('addSteps');
+let removeSteps = document.getElementById('removeSteps');
 
 addSteps.addEventListener('click', (event) => {
   event.preventDefault();
   let clone = steps.cloneNode(true);
   clone.value = '';
   stepsContainer.appendChild(clone);
+})
+
+removeSteps.addEventListener('click', (event) => {
+  event.preventDefault();
+  stepsContainer.removeChild(stepsContainer.lastChild);
 })
